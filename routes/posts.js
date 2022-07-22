0787a0bd82f4cbc6418cf67dbc0f37aa5139cf05
sfkse <sfkse5591@gmail.com>
2022-07-22 18:31:25 +0200

    Render posts and post detail

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
 
+router.get('/new', (req, res) => {
+    res.render('site2/addpost');
+})
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id).then(post => {
+        res.render('site2/post', {post: post.toJSON()});
+    })
+})
+
 router.post('/test', (req, res) => {
     Post.create(req.body)
     res.redirect('/');
