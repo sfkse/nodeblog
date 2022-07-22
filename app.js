d2638d2e7e3ed5c7d854bd73c9b9267eb84cc6f3
sfkse <sfkse5591@gmail.com>
2022-07-22 17:43:13 +0200

    Create addpost page

--- app.js
+++ app.js
@@ -1,73 +1,23 @@
-const exphbs = require('express-handlebars')
-const path = require('path')
-const express = require('express')
+const exphbs = require('express-handlebars');
+const path = require('path');
+const express = require('express');
 const mongoose = require('mongoose');
-const app = express()
-const port = 3000
-const hostname = '127.0.0.1'
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
 
+app.use(express.static('public'));
 
-app.use(express.static('public'))
-
- mongoose.connect('mongodb://127.0.0.1:27017/nodeblog_db');
+mongoose.connect('mongodb://127.0.0.1:27017/nodeblog_db');
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('site2/index');
-})
+const main = require('./routes/main');
 
-app.get('/about', (req, res) => {
-    res.render('site2/about');
-})
-
-app.get('/blog', (req, res) => {
-    res.render('site2/blog');
-})
-
-app.get('/contact', (req, res) => {
-    res.render('site2/contact');
-})
-
-app.get('/login', (req, res) => {
-    res.render('site2/login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('site2/register');
-})
+app.use('/', main);
 
 app.listen(port, hostname, () => {
-  console.log(`Server calisiyor, http://${hostname}:${port}/`)
-})
-
-
-// const http = require('http')
-// const fs = require('fs')
-
-// const hostname = "127.0.0.1";
-// const port = 3000;
-
-// const indexPage = fs.readFileSync('index.html')
-// const aboutPage = fs.readFileSync('about.html')
-// const contactPage = fs.readFileSync('contact.html')
-// const notFound = fs.readFileSync('404.html')
-// const server = http.createServer((req, res) => {
-// if (req.url === '/') {
-//     return res.end(indexPage);
-    
-// }else if (req.url === '/about') {
-//     return res.end(aboutPage)
-// }else if (req.url === '/contact') {
-//     return res.end(contactPage)
-// }else{
-//     res.statusCode = 404;
-//     res.end(notFound)
-// }
-// })
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server calisiyor, http://${hostname}:${port}/`);
-// })
+  console.log(`Server is running, http://${hostname}:${port}/`);
+});
---
+++ routes/main.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('site2/index');
+})
+
+router.get('/about', (req, res) => {
+    res.render('site2/about');
+})
+
+router.get('/blog', (req, res) => {
+    res.render('site2/blog');
+})
+
+router.get('/contact', (req, res) => {
+    res.render('site2/contact');
+})
+
+router.get('/login', (req, res) => {
+    res.render('site2/login');
+})
+
+router.get('/register', (req, res) => {
+    res.render('site2/register');
+})
+
+router.get('/posts/new', (req, res) => {
+    res.render('site2/addpost');
+})
+
+router.post('/posts/test', (req, res) => {
+    res.redirect('/');
+})
+module.exports = router;
