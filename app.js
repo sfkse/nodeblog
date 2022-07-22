2c0fd61ea15a2b6ccb31fe123ac20dd99ffe50ce
sfkse <sfkse5591@gmail.com>
2022-07-22 22:21:51 +0200

    Upload picture

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload')
 const app = express();
 const port = 3000;
 const hostname = '127.0.0.1';
 
-app.use(express.static('public'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/nodeblog_db');
+
+app.use(express.static('public'));
+
+app.use(fileUpload());
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
